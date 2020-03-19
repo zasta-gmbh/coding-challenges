da8af8ef1d05f7ef543ffeb9b30046f39e32e278
KleinerNull <anstecknadel23@yahoo.de>
2020-03-19 01:39:28 +0100

    some tests

--- laravel/tests/Unit/OrderDashboardTest.php
+++ laravel/tests/Unit/OrderDashboardTest.php
@@ -5,12 +5,26 @@ namespace Tests\Unit;
 
 
 use App\Libs\OrderDashboard;
+use App\Customer;
+use App\Order;
 use Tests\TestCase;
 
 class OrderDashboardTest extends TestCase
 {
+
     public function testDashboard(){
         $dashboard = new OrderDashboard;
-        $dashboard->abcAnalysis();
+        $analysis = $dashboard->abcAnalysis();
+
+        $totalCustomers = Customer::count();
+        $this->assertEquals(count($analysis['top20Customers']), intval($totalCustomers * 0.2));
+
+        $actualShare = $dashboard->shareOfTotalRevenue($analysis['top80PercentCustomers']);
+        $this->assertGreaterThanOrEqual(0.8, $actualShare);
+        $actualShareDB = $dashboard->shareOfTotalRevenue($analysis['top80PercentCustomersDB']);
+        $this->assertGreaterThanOrEqual(0.8, $actualShareDB);
+
+        $this->assertEquals(count($analysis['top80PercentCustomersDB']), count($analysis['top80PercentCustomers']));
+
     }
 }
